c4ada93d7795eea11972eb17cfe669a0cdcd0d96
Priyanshu1112 <ashu1112sing@gmail.com>
2024-04-16 03:02:45 +0530

    j

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Filter from "./components/Filter";
 
 function App() {
   return (
-    <div className="w-full h-full flex p-5 overflow-hidden">
+    <div className="w-full h-full flex p-5 overflow-hidde">
       <Filter />
       <Creative />
     </div>
